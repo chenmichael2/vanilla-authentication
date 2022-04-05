00acb34fbd56057185f38f978c292ee9bbd259c1
chenmichael2 <chenmichael2012@gmail.com>
2022-04-05 16:47:13 -0700

    adding more consolelogs to read more

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const users = [
     {
         name: "name",
@@ -16,6 +18,10 @@ app.post('/users', (req, res) => {
         name: req.body.name,
         password: req.body.password
     }
+    console.log(req.body)
+    console.log(user);
+    users.push(user);
+    res.status(201).send();
 
 })
 
